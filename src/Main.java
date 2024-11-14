b20ddb4213f980c78b582b2b1c50ebeabb18b7d7
Desmond Roche <desmondroche@gmail.com>
2024-11-14 17:07:40 -0500

    Circle area code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        double radius;
+        double area;
+
+        //Assign a radius
+        radius = 20;
+
+        //Compute area
+        area = Math.PI * radius * radius;
+
+        //Display results
+        System.out.println("The area for the circle of radius " + radius + " is " + area);
     }
 }
